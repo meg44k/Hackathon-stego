9d0310c5e570c2589912a044a98e8b573995da94
Ruyurl <ruyurl0707@gmail.com>
2024-06-15 22:24:09 +0900

    min-heightの修正，UPのファイル名とprev

---
+++ flask_app/static/js/index_animation.js
@@ -0,0 +1,11 @@
+// ロード時にアニメーション
+window.addEventListener('load', function() {
+	const textElementTit = document.querySelector('.page-title-index');
+	textElementTit.style.opacity = 1;
+	const textElementPro = document.querySelector('.project-name');
+	textElementPro.style.opacity = 1;
+	const textElementExp = document.querySelector('.project-explain');
+	textElementExp.style.opacity = 1;
+	const textElementBut = document.querySelector('.button');
+	textElementBut.style.opacity = 1;
+});
