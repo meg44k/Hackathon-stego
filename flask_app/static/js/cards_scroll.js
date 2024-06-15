d333b9d06f4d763505cd34cfae5a53da4c33298f
Ruyurl <ruyurl0707@gmail.com>
2024-06-15 15:24:52 +0900

    単語帳画面追加

--- flask_app/static/js/cards_scroll.js
+++ flask_app/static/js/cards_scroll.js
@@ -1,40 +1,63 @@
-//親要素取得  
-var element = document.getElementById("jsnap");
-
-//子要素を参照するための番号
-var num = 0;
-
-// next
 function next() {
-	if(num+1 < element.children.length) {
-		var child = element.children[num+1];  
-		child.scrollIntoView(  
-		{
-			behavior:"smooth",
-			block:"nearest",
-			inline:"nearest",
-		});
-		num++;
-	}
-	else {
+    if (words.length === 0) return;
+	if (currentIndex+1 < words.length) {
+		currentIndex++;
+	} else {
 		return;
 	}
+    showingEnglish = true;
+    updateCard();
 }
 
-
-// prev
 function prev() {
-	if(num-1 >= 0) {
-		var child = element.children[num-1];  
-		child.scrollIntoView(  
-		{
-			behavior:"smooth",
-			block:"nearest",
-			inline:"nearest",
-		});
-		num--;
-	}
-	else {
+    if (words.length === 0) return;
+	if (currentIndex-1 >= 0) {
+		currentIndex--;
+	} else {
 		return;
 	}
+    showingEnglish = true;
+    updateCard();
 }
+
+
+// //親要素取得  
+// var element = document.getElementById("flashcard");
+
+// //子要素を参照するための番号
+// var num = 0;
+
+// // next
+// function next() {
+// 	if(num+1 < element.children.length) {
+// 		var child = element.children[num+1];  
+// 		child.scrollIntoView(  
+// 		{
+// 			behavior:"smooth",
+// 			block:"nearest",
+// 			inline:"nearest",
+// 		});
+// 		num++;
+// 	}
+// 	else {
+// 		return;
+// 	}
+// }
+
+
+// // prev
+// function prev() {
+// 	if(num-1 >= 0) {
+// 		var child = element.children[num-1];  
+// 		child.scrollIntoView(  
+// 		{
+// 			behavior:"smooth",
+// 			block:"nearest",
+// 			inline:"nearest",
+// 		});
+// 		num--;
+// 	}
+// 	else {
+// 		return;
+// 	}
+// }
